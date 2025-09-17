f70d1c2ad900ba105e7d01ca23f228e43a9bcd55
Huduong123 <nguyentuanh097888@gmail.com>
2025-09-17 21:53:41 +0700

    exception

---
+++ order-service/src/main/java/io/github/Huduong123/order_service/exception/DuplicateResourceException.java
@@ -0,0 +1,8 @@
+package io.github.Huduong123.order_service.exception;
+public class DuplicateResourceException extends RuntimeException{
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+    
+}
